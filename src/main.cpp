a8489374fc24be4036db8ba3ea0815d5d4781639
newtonshh <780029565@qq.com>
2024-02-03 20:30:05 +0800

    使用main函数引用add函数

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,8 @@ void print_all(int x)
 
 int main()
 {
-    int a = 1, b =2, c = 3;
-    thread t1(print_all, a);
-    thread t2(print_all, b);
-    thread t3(print_all, c);
-    add(1,2);
+    int x = add(1,2);
+    cout << x << endl;
 
     return 0;
 }
